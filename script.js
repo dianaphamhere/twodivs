10d78deaff2e67a3ab2cf8a580f8358ede4c384b
Diana Pham <dianaphamis@gmail.com>
2018-11-30 23:35:36 -0800

    - Added golfball and tee - Cleaned up a bit of code

--- script.js
+++ script.js
@@ -3,24 +3,18 @@ var two = document.getElementById("two");
 var btn = document.getElementById("btn");
 var thing = document.getElementById("thing");
 
-// btn.addEventListener("click", function(){
-// 	one.setAttribute("id", "aEye");
-// 	two.setAttribute("id", "bEye");
+var zero = {name:"square and circle", a:"one", b:"two"};
+var ic = {name:"ice cream", a:"aIC", b:"bIC"};
+var eye = {name:"eye", a:"aEye", b:"bEye"};
+var chick = {name:"chick", a:"aChick", b:"bChick"};
+var gt = {name:"golfball and tee", a:"aGT", b:"bGT"};
+var arr = [zero, ic, eye, chick, gt];
 
-// 	thing.innerHTML = "ice cream";
-// })
-
-var zero = {index:0, name:"square and circle", a:"one", b:"two"};
-var ic = {index:1, name:"ice cream", a:"aIC", b:"bIC"};
-var eye = {index:2, name:"eye", a:"aEye", b:"bEye"};
-var chick = {index:3, name:"chick", a:"aChick", b:"bChick"};
-var arr = [zero, ic, eye, chick];
-console.log(arr[0].name);
 
 btn.addEventListener("click", nextNum);
 
 function nextNum(){
-	var ranNum = Math.floor(Math.random() * 4);
+	var ranNum = Math.floor(Math.random() * arr.length);
 	console.log(ranNum);
 
 	one.setAttribute("id", arr[ranNum].a);

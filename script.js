deb54a78155e147db075a8019c467a0c98f22a6b
Diana Pham <dianaphamis@gmail.com>
2018-11-30 21:10:18 -0800

    - Created chick - Created js objects - Created array of objects

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ var btn = document.getElementById("btn");
 var thing = document.getElementById("thing");
 
 btn.addEventListener("click", function(){
-	one.setAttribute("id", "aIC");
-	two.setAttribute("id", "bIC");
+	one.setAttribute("id", "aEye");
+	two.setAttribute("id", "bEye");
 
 	thing.innerHTML = "ice cream";
 })
+
+var ic = {index:1, name:"ice cream", a:"aIC", b:"bIC"};
+var eye = {index:2, name:"eye", a:"aEye", b:"bEye"};
+var chick = {index:2, name:"chick", a:"aChick", b:"bChick"};
+var arr = [ic, eye, chick];
+console.log(arr[0].name);
